a229817efa0b6c3d5fcab080d9d1d5b7f0ce41be
ajSeadler <aj6287@gmail.com>
2024-12-03 16:48:14 -0600

    no longer hide nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,16 +5,15 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   const [isVisible, setIsVisible] = useState(true);
+  const [activeLink, setActiveLink] = useState("home");
   const lastScrollY = useRef(window.scrollY);
 
   useEffect(() => {
     const handleScroll = () => {
       if (window.scrollY > lastScrollY.current) {
-        // Scrolling down
-        setIsVisible(false); //keep this as true until animation doesnt happen on mobile. animation should only happen on desktop
+        setIsVisible(true); // false if you want to hide nav bar
       } else {
-        // Scrolling up
-        setIsVisible(true);
+        setIsVisible(true); // Show navbar on scroll up.
       }
       lastScrollY.current = window.scrollY;
     };
@@ -28,31 +27,51 @@ export default function NavBar() {
     <nav className={`navbar ${isVisible ? "visible" : "hidden"}`}>
       <ul className="nav-links">
         <li>
-          <Link to="/" className="nav-item">
+          <Link
+            to="/"
+            className={`nav-item ${activeLink === "home" ? "active" : ""}`}
+            onClick={() => setActiveLink("home")}
+          >
             <FiHome className="icon" />
             <span>Home</span>
           </Link>
         </li>
         <li>
-          <a href="#about" className="nav-item">
+          <a
+            href="#about"
+            className={`nav-item ${activeLink === "about" ? "active" : ""}`}
+            onClick={() => setActiveLink("about")}
+          >
             <FiUser className="icon" />
             <span>About</span>
           </a>
         </li>
         <li>
-          <a href="#projects" className="nav-item">
+          <a
+            href="#projects"
+            className={`nav-item ${activeLink === "projects" ? "active" : ""}`}
+            onClick={() => setActiveLink("projects")}
+          >
             <FiCode className="icon" />
             <span>Projects</span>
           </a>
         </li>
         <li>
-          <a href="#blog" className="nav-item">
+          <a
+            href="#blog"
+            className={`nav-item ${activeLink === "blog" ? "active" : ""}`}
+            onClick={() => setActiveLink("blog")}
+          >
             <FiFileText className="icon" />
             <span>Blog</span>
           </a>
         </li>
         <li>
-          <a href="#contact" className="nav-item">
+          <a
+            href="#contact"
+            className={`nav-item ${activeLink === "contact" ? "active" : ""}`}
+            onClick={() => setActiveLink("contact")}
+          >
             <FiMail className="icon" />
             <span>Contact</span>
           </a>
